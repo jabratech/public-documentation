366e512c68c42a45ff7b4d8b797874100ab38580
KYLE <kyleniosco@gmail.com>
2024-01-02 18:23:10 +0800

    Updated Config 2.0

--- jabra-docs/docusaurus.config.js
+++ jabra-docs/docusaurus.config.js
@@ -13,14 +13,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://jabratech-docs.github.io',
+  url: 'https://jabratech.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/public-documentation/',
+  // URL: https://jabratech.github.io/public-documentation/
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'jabratech-docs', // Usually your GitHub org/user name.
+  organizationName: 'jabratech', // Usually your GitHub org/user name.
   projectName: 'public-documentation', // Usually your repo name.
 
   onBrokenLinks: 'ignore',
